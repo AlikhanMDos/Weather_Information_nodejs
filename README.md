454b97edd03d7017cc0c1ddda936bb6764fdc4b7
AlikhanMDos <122694328+AlikhanMDos@users.noreply.github.com>
2024-01-29 18:51:53 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Weather_Information_nodejs
-Example of using node.js to create weather website
+# Weather App
+
+Weather App is a web application that provides users with current weather information, extended forecasts, and the latest news for a specified city.
+
+
+## Description
+
+Weather App is designed to offer users a comprehensive overview of the weather conditions in a given city. It fetches real-time weather data, extended forecasts for the next 14 days, and the latest news related to the specified location.
+
+## Features
+
+- Real-time weather information
+- 14-day extended weather forecast
+- Latest news related to the specified city
+
+## Installation
+
+To install the Weather App, follow these steps:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/weather-app.git
+   cd weather-app
+
+
+#### How to Obtain API Key (example):
+1. Visit [OpenWeatherMap](https://openweathermap.org/) and sign up for a free account.
+2. After signing in, navigate to the "API Keys" section to obtain your API key.
+![Alt text](image.png)
+
+
+## API's
+### OpenWeather API
+- [OpenWeather](https://openweathermap.org/price) provides real-time weather data.
+ 
+### Visual Crossing Weather API
+- [Visual Crossing Weather](https://www.visualcrossing.com/weather-data-editions) offers a 14-day weather forecast.
+
+### Google Maps API
+- Google Maps is used to display the location of the city on the map.
+
+### News API
+- [News API](https://newsapi.org/) provides news articles based on the specified city.
---
+++ route/index.js
@@ -0,0 +1,92 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const NewsAPI = require('newsapi');
+
+const app = express();
+const OPENWEATHERMAP_API_KEY = '709d0ff50ae9221f8ae9916496ba77cc';
+const VISUAL_CROSSING_API_KEY = '3A5QWCLECANGUPR99GW52YWU9';
+const NEWS_API_KEY = 'ac3fbb6249c34799a0b6fc51bee6df46';
+
+const newsapi = new NewsAPI(NEWS_API_KEY);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+const defaultCity = 'Astana';
+
+const defaultWeatherData = {
+    city: defaultCity,
+    
+};
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.post('/weather', { city: defaultCity });
+
+        const { weatherData, forecastData, newsData } = response.data;
+
+        res.render('index.ejs', { weatherData, forecastData, newsData, error: null });
+    } catch (error) {
+        console.error(error);
+        res.render('index.ejs', { weatherData: null, forecastData: null, newsData: null, error: 'Error fetching data' });
+    }
+});
+
+app.post('/weather', async (req, res) => {
+    let city = req.body.city || "Astana"; // Убедитесь, что city не равно undefined
+
+    try {
+        const response = await axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHERMAP_API_KEY}`
+        );
+
+        const weatherData = {
+            cityId: response.data.id,
+            city: response.data.name,
+            temperature: response.data.main.temp,
+            feelsLike: response.data.main.feels_like,
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            coordinates: response.data.coord,
+            humidity: response.data.main.humidity,
+            pressure: response.data.main.pressure,
+            windSpeed: response.data.wind.speed,
+            countryCode: response.data.sys.country,
+            rainVolume: response.data.rain ? response.data.rain['1h'] : 0,
+        };
+
+        const visualCrossingResponse = await axios.get(
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${VISUAL_CROSSING_API_KEY}`
+        );
+
+        const forecastData = visualCrossingResponse.data;
+
+        const newsResponse = await newsapi.v2.topHeadlines({
+            q: city, // Добавляем параметр q (query), который указывает город для новостей
+        });
+
+        if (newsResponse.status === 'ok') {
+            const newsData = newsResponse.articles;
+            console.log('News API Response:', newsResponse);
+            res.render('index.ejs', { weatherData, forecastData, newsData, error: null });
+        } else {
+            console.error('News API Error:', newsResponse.message);
+            res.render('index.ejs', { weatherData, forecastData, newsData: null, error: newsResponse.message });
+        }
+    } catch (error) {
+        console.error(error);
+        res.render('index.ejs', { weatherData: null, forecastData: null, newsData: null, error: error.message });
+    }
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
